4645c6553ab8f7096de18a148f3d5b1653bc82cb
mantepran <mantepranckunaite@gmail.com>
2025-09-20 21:31:07 +0300

    Pridėtas medianos pasirinikimas ir valdymas netinkamos įvesties
    Vietoj galutinio balo skaičiavimo, pateikiant vidurkį, pridėta galimybė naudoti ir medianą. Naudotojas pats gali pasirinkti norimą būdą. Taip pat įvedus netinkamą pažymių kiekį, nutraukiamas procesas ir pranešama, jog yra klaida.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 using std::cout;
 using std::cin;
@@ -19,8 +20,19 @@ struct Studentas{
     string pav;
     vector<int> paz;
     int egzas;
-    float rez;
     };
+float mediana(vector<int> &paz) {
+    sort(paz.begin(), paz.end());
+    int dyd = paz.size();
+    if (dyd == 0) {
+        return 0;
+    }
+    if (dyd % 2 == 0) {
+        return (paz[dyd / 2 - 1] + paz[dyd / 2]) / 2.0;
+    } else {
+        return paz[dyd / 2];
+    }
+}
 
 int main () {  
     Studentas Laik;
@@ -28,20 +40,53 @@ int main () {
     cout<<"Iveskite varda: "; cin>>Laik.vard;
     cout<<"Iveskite pavarde: "; cin>>Laik.pav;
     cout<<"Kiek pazymiu turi studentas? "; cin>>n;
+    if (n <= 0) {
+        cout<<"Klaida. Studentas turi turėti bent vieną pažymį!"<<endl;
+        return 1;
+    }
     for (int i=0;i<n;i++){
         cout<<"Iveskite "<<i+1<< "paz. is "<<n<<" : "; cin>>m;
         Laik.paz.push_back(m);
         sum+=m;
     }
-    cout<<"Iveskite egzamina: "; cin>>Laik.egzas;
-    Laik.rez= Laik.egzas*0.6 + double(sum)/double(Laik.paz.size()) *0.4;
+    cout<<"Iveskite egzamina: "; cin>>Laik.egzas; cout<<endl;
+    
+    float vid = (float)(sum) / n;
+    float med = mediana(Laik.paz);
+    float rezVid = Laik.egzas * 0.6 + vid * 0.4;
+    float rezMed = Laik.egzas * 0.6 + med * 0.4;
 
-    cout << fixed << setprecision(2);
-    cout << left << setw(15) << "Vardas"
-     << left << setw(15) << "Pavarde"
-     << right << setw(15) << "Galutinis (vid.)" << endl;
-    cout << "------------------------------------------" << endl;
-    cout << left << setw(15) << Laik.vard
-     << left << setw(15) << Laik.pav
-     << right << setw(15) << Laik.rez << endl;
+    cout<<"Pasirinkite galutinio balo skaiciavimo buda: "<<endl;
+    cout<<"1 - Vidurkis"<< endl<<"2 - Mediana"<<endl<<"3 - Abu"<<endl;
+
+    int pasirinkimas;
+    cin>>pasirinkimas;
+
+    cout<<endl<<left<<setw(10)<<"Vardas"<<setw(10)<<"Pavarde";
+
+    if (pasirinkimas == 1) {
+        cout<<"Galutinis (Vid.)"<<endl;
+        cout<<"------------------------------------------"<<endl;
+        cout << left << setw(10) << Laik.vard
+            << setw(10) << Laik.pav
+            << setw(20) << fixed << setprecision(2) << rezVid << endl;
+
+    } else if (pasirinkimas == 2) {
+        cout << "Galutinis (Med.)" << endl;
+        cout << "------------------------------------------" << endl;
+        cout << left << setw(10) << Laik.vard
+             << setw(10) << Laik.pav
+             << setw(20) << fixed << setprecision(2) << rezMed << endl;
+
+    } else if (pasirinkimas == 3) {
+        cout << "Galutinis (Vid.) / Galutinis (Med.)" << endl;
+        cout << "------------------------------------------" << endl;
+        cout << left << setw(10) << Laik.vard
+             << setw(10) << Laik.pav
+             << setw(20) << fixed << setprecision(2) << rezVid
+             << setw(20) << fixed << setprecision(2) << rezMed << endl;
+
+    } else {
+        cout << "Klaida. Pasirinktas netinkamas simbolis" << endl;
+    }
 }
