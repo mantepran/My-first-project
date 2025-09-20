3d00553a5ab963bcfc7879f3a7df317f65f7bbee
mantepran <mantepranckunaite@gmail.com>
2025-09-20 11:57:41 +0300

    Pirma versija, kurioje nuskaitomi duomenys ir pateikiami rezultatai

--- main.cpp
+++ main.cpp
@@ -1,8 +1,38 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <string>
 
-int main()
-{
-  cout<<"Hello world!!!";
-  return 0;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+using std::setw;
+using std::left;
+using std::right;
+
+struct Studentas{
+    string vard;
+    string pav;
+    vector<int> paz;
+    int egzas;
+    float rez;
+    };
+
+int main () {  
+    Studentas Laik;
+    int sum = 0, n, m;
+    cout<<"Iveskite varda: "; cin>>Laik.vard;
+    cout<<"Iveskite pavarde: "; cin>>Laik.pav;
+    cout<<"Kiek pazymiu turi studentas? "; cin>>n;
+    for (int i=0;i<n;i++){
+        cout<<"Iveskite "<<i+1<< "paz. is "<<n<<" : "; cin>>m;
+        Laik.paz.push_back(m);
+        sum+=m;
+    }
+    cout<<"Iveskite egzamina: "; cin>>Laik.egzas;
+    Laik.rez= Laik.egzas*0.6 + double(sum)/double(Laik.paz.size()) *0.4;
+    
+    cout<<Laik.vard<<" | "<<Laik.pav<<" | "<<Laik.rez<<endl;
 }
